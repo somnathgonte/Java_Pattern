7d5e88ee00ef17fecf7c11c68bef3cecbcee11a0
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-03-12 18:05:20 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,29 @@
+/* 
+    Input : iRow = 4 iCol = 4
+    Output : * * * *
+             * * * *
+             * * * *
+             * * * *
+*/
+
+import java.util.*;
+public class Pattern1
+{
+    public static void main(String []arg)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter rows and columns: ");
+      int iRow = sc.nextInt();
+      int iCol= sc.nextInt();
+
+      for(int i=1; i<=iRow; i++)
+      {
+        for(int j=1; j<=iCol; j++)
+        {
+            System.out.print("*"+" ");
+        }
+        System.out.println(" ");
+      }
+
+    }
+}
