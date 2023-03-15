c495a81ce18a5ff5ba5f672606752d1846ab140d
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-03-15 23:29:09 +0530

    Add files via upload

---
+++ StarPattern9.java
@@ -0,0 +1,22 @@
+/*
+           ****
+           ****
+           ****
+           ****
+
+ */
+public class StarPattern9 
+{
+    public static void main(String []arg)
+    {
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=1; j<=4;j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
